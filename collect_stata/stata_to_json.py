ea519ff1d1c4b4e6aeef086f041d59b29e9ed78a
afuetterer <fuetterh@posteo.de>
2019-09-05 16:20:52 +0200

    Use multiprocessing to handle multiple files at once

--- collect_stata/stata_to_json.py
+++ collect_stata/stata_to_json.py
@@ -1,7 +1,21 @@
 """stata_to_json.py"""
 __author__ = "Marius Pahl"
 
+import logging
+import time
+from multiprocessing import Process
+from pathlib import Path
+
 from .dataset import Dataset
+
+
+def run(file: Path, output_path: Path, study_name: str) -> None:
+    """Helper function that runs in parallel."""
+    dataset = Dataset()
+    dataset.read_stata(str(file))
+    dataset.write_json(
+        output_path.joinpath(file.stem).with_suffix(".json"), study=study_name
+    )
 
 
 def stata_to_json(study_name, input_path, output_path):
@@ -15,8 +29,17 @@ def stata_to_json(study_name, input_path, output_path):
     After this, it writes it out as csv and json files.
     """
 
+    # gather the processes
+    start_time = time.time()
+    processes = []
     for file in input_path.glob("*.dta"):
-        dataset = Dataset()
-        fname = file.stem
-        dataset.read_stata(str(file))
-        dataset.write_json(str(output_path) + "/" + fname + ".json", study=study_name)
+        process = Process(target=run, args=(file, output_path, study_name))
+        processes.append(process)
+        process.start()
+
+    # complete the processes
+    for process in processes:
+        process.join()
+
+    duration = time.time() - start_time
+    logging.info(f"Duration {duration:.5f} seconds")
