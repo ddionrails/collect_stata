53d877ceb36f50da98694ed0a0c4a46199a37f3e
hansendx <dhansen@diw.de>
2019-09-26 09:28:20 +0200

    Add test_read_stata skeleton

---
+++ tests/test_read_stata.py
@@ -0,0 +1,16 @@
+# pylint: disable=all
+
+import unittest
+
+import pandas
+
+from collect_stata.read_stata import extract_category_value_labels
+
+
+class TestMetadataFunctions(unittest.TestCase):
+    pass
+
+
+# @pytest.fixture(name="umlaut_dataframe")
+# def _umlaut_dataframe(request):
+#    pass
