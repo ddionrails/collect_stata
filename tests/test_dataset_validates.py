0e34f0db12f3d30a064fec50c364640dbde384b4
hansendx <dhansen@diw.de>
2019-10-09 16:26:32 +0200

    [close #48] Fix encoding issues
    Problem
    -------
    
    Pandas decodes data with Latin-1
    which lead to problems with german umlauts.
    
    Solution
    --------
    
    * Data is now encoded back to Latin-1
      to minimize en/decoding errors.
    * Add flag `--latin1` and `-l` to demarcate that the source files are
      Latin-1 encoded.
    
    Explanation
    -----------
    
    We have two expected cases, keeping in mind
    that utf-8 output is desired.
    
    1. Input files are utf-8 encoded: decoding with subsequent encoding
       in Latin-1 will moth likely leave the original utf-8 encoding intact.
    2. Input files are Latin-1 or Windows-1252 encoded: pandas decodes these
       files correctly, which means that we are working with utf-8 strings
       in the pandas object. This can then safely encoded to utf-8.
    
    We also have one case that cannot be controlled for:
    
    If the input is encoded in neither one of the expected encodings,
    Latin-1 de and encoding will probably also leave this encoding intact.
    Since, at the moment, pandas StataReader decoding is
    hard coded to Latin-1, one Latin-1 decode, encode circle is necessary.
    Offering more flexibility in the input encoding would necessitate
    rereading the output in the correct encoding and writing it back to
    utf-8 which just complicates the process and slows it down.
    Input should generally be kept utf-8 encoded.

--- tests/test_dataset_validates.py
+++ tests/test_dataset_validates.py
@@ -3,20 +3,29 @@
 
 import json
 import pathlib
-from typing import Dict
+from typing import Any, Dict
 
+from _pytest import tmpdir as pytest_tmpdir
 from jsonschema import validate
 
-from collect_stata.__main__ import stata_to_json
+from collect_stata.__main__ import StataToJson
 
 
-def test_dataset_validates(tmpdir, dataset_schema: Dict):
+def test_dataset_validates(tmpdir: pytest_tmpdir, dataset_schema: Dict[str, Any]) -> None:
     """ Functional test that validates a resulting dataset against a JSON Schema """
 
     input_path = pathlib.Path("tests/input")
     output_path = pathlib.Path(str(tmpdir.mkdir("subdir")))
 
-    stata_to_json(study_name="soep-core", input_path=input_path, output_path=output_path)
+    # The test.dta file is encoded in Latin-1 or Windows-1252
+
+    stata_to_json = StataToJson(
+        study_name="soep-core",
+        input_path=input_path,
+        output_path=output_path,
+        latin1=True,
+    )
+    stata_to_json.parallel_run()
 
     with open(output_path.joinpath("test.json")) as json_file:
         result = json.load(json_file)
